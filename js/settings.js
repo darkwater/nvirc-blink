9a453047a2a64634467ddf09487b53da5c8e5ba8
Darkwater124 <Darkwater124@gmail.com>
2014-05-09 22:22:10 +0200

    Updated settings window

--- js/settings.js
+++ js/settings.js
@@ -17,11 +17,14 @@ document.body.onkeypress = function (a)
 var xml = fs.readFileSync('data/settings.xml');
     $xml = $( $.parseXML(xml.toString()) ).children(0);
 
-$xml.children().each(function (si, section)
+$xml.children().each(function (_, sectiondata)
 {
+    sectiondata = $(sectiondata);
+
     $('#settings-menu').append
     (
-        $('<li />').text($(section).attr('label'))
+        $('<li />').text($(sectiondata).attr('label'))
+            .prepend($('<i />').addClass('fa fa-fw fa-' + $(sectiondata).attr('icon')))
             .click(function ()
             {
                 $('#settings-menu li.active').removeClass('active');
@@ -29,10 +32,43 @@ $xml.children().each(function (si, section)
 
                 var main = $('main').html('');
 
-                $(section).find('fieldset').each(function (fi, set)
+                $(sectiondata).find('fieldset').each(function (_, setdata)
                 {
-                    var fieldset = $('<fieldset />').appendTo(main)
-                        .append($('<legend />').text($(set).attr('label')));
+                    setdata = $(setdata);
+
+                    var fieldset = $('<section />').appendTo(main)
+                        .append($('<h2 />').text(setdata.attr('label')));
+
+                    setdata.children().each(function (_, fielddata)
+                    {
+                        fielddata = $(fielddata);
+
+                        var fieldhtml = $('#f-' + setdata.attr('layout') +'-'+ fielddata[0].tagName.toLowerCase()).html();
+                        if (!fieldhtml) return;
+
+                        fieldhtml = fieldhtml.replace(/%[A-Z]+%/g, function (attr)
+                        {
+                            attr = attr.match(/[A-Z]+/)[0].toLowerCase();
+
+                            if (attr == 'curvalue') return '';
+
+                            return fielddata.attr(attr) || '';
+                        });
+
+                        var field = $(fieldhtml);
+
+                        switch (fielddata[0].tagName)
+                        {
+                            case 'string':
+
+                                if (fielddata.attr('masked'))
+                                    field.find('input').attr('type', 'password');
+
+                                break;
+                        }
+
+                        fieldset.append(field);
+                    });
                 });
             })
     );
